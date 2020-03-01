f734ae69ac819704f6095e5a99da9fc28a45a3f3
atolmid <atolmid@gmail.com>
2020-03-01 19:48:24 +0000

    Added cpuUtilization_ initialization to constructor, implementation for methods UpTime(), Ram(), and Pid()

--- src/process.cpp
+++ src/process.cpp
@@ -11,25 +11,30 @@ using std::to_string;
 using std::vector;
 
 //class constructor
-Process::Process(int pid):pid_(pid){}
+Process::Process(int pid):pid_(pid){ 
+    cpuUtilization_ = Process::CpuUtilization();
+}
 
 // TODO: Return this process's ID
 int Process::Pid() { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+float Process::CpuUtilization() { 
+
+    return 0; 
+}
 
 // TODO: Return the command that generated this process
 string Process::Command() { return string(); }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() { return LinuxParser::Ram(pid_); }
 
 // TODO: Return the user (name) that generated this process
 string Process::User() { return string(); }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
