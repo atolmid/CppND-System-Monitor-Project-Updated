763adb0ba8f8110411605b4ae694361807f8bac0
atolmid <atolmid@gmail.com>
2020-03-01 19:55:08 +0000

    Removed the [maybe_unused] from the Uid(pid) method

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -233,7 +233,7 @@ string LinuxParser::Ram(int pid) {
 
 // TODO: Read and return the user ID associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Uid(int pid[[maybe_unused]]) { 
+string LinuxParser::Uid(int pid) { 
   string key, value;
   string line;
   std::ifstream stream(kProcDirectory + to_string(pid) + kStatusFilename);
