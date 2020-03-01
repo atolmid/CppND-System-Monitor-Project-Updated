fd3dc4d5dbbeeab0e8505401a8e773de08e001b8
atolmid <atolmid@gmail.com>
2020-03-01 21:00:30 +0000

    Added impleentation for method Command(pid)

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -210,7 +210,15 @@ int LinuxParser::RunningProcesses() {
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Command(int pid) { 
+  string command = "";
+  std::ifstream stream(kProcDirectory + to_string(pid) + kCmdlineFilename);
+  if (stream.is_open()){
+    std::getline(stream, command);
+    return command;
+  } 
+  return string(); 
+  }
 
 // Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
